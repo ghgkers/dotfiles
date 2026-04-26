7092afef0a2f5737ddece34df55add91558f6377
dx3d <chayukov1024@gmail.com>
2026-04-26 18:24:57 +0000

    update: 2026-04-26 18:24

--- configuration.nix
+++ configuration.nix
@@ -8,18 +8,18 @@ networking.hostName = "nix";
 networking.networkmanager.enable = true;
 nixpkgs.config.allowUnfree = true;
 services.xserver.videoDrivers = [ "nvidia" ];
+services.asusd.enable = true;
+services.asusd.enableUserService = true;
 services.xserver.xkb = {
 layout = "us,ru";
 variant = "";
-options = "grp:win_space_toggle"; # Переключение по Super + Space
-  };
-
-  # Чтобы в консоли (TTY) тоже был русский шрифт
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us"; # Или "ru", если хочешь русский в консоли по умолчанию
-  };
+options = "grp:win_space_toggle";
+};
+i18n.defaultLocale = "en_US.UTF-8";
+console = {
+font = "Lat2-Terminus16";
+keyMap = "us";
+};
 hardware.nvidia.open = true;
 environment.sessionVariables = {
 WLR_NO_HARDWARE_CURSORS = "1";
@@ -32,9 +32,9 @@ packages = with pkgs; [kitty hyprland waybar st dmenu vim asusctl fastfetch vesk
 };
 programs.hyprland.enable = true;
 programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+enable = true;
+remotePlay.openFirewall = true;
+dedicatedServer.openFirewall = true;
   };
 services.flatpak.enable = true;
 system.stateVersion = "25.11";
@@ -49,9 +49,9 @@ programs.bash.shellAliases = {
 dotsync = "cd ~/dotfiles && sudo cp /etc/nixos/configuration.nix . && sudo cp /etc/nixos/hardware-configuration.nix . && cp -r ~/.config/hypr . && cp -r ~/.config/waybar . && git add . && git commit -m \"update: $(date +'%Y-%m-%d %H:%M')\" && git push origin main && cd -";
 };
 services.libinput = {
-  enable = true;
-  touchpad.disableWhileTyping = true;
-  touchpad.additionalOptions = ''Option "Ignore" "on"'';
+enable = true;
+touchpad.disableWhileTyping = true;
+touchpad.additionalOptions = ''Option "Ignore" "on"'';
 };
 }
 
