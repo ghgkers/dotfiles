8f6dd9b4ad2e8476d0f2c5a9fd9d42dd0f6e8d16
dx3d <chayukov1024@gmail.com>
2026-04-28 10:10:09 +0000

    update: 2026-04-28 10:10

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@ services.asusd={enable=true;enableUserService=true;};
 i18n.defaultLocale="en_US.UTF-8";
 console={font="Lat2-Terminus16";keyMap="us";};
 environment.sessionVariables={WLR_NO_HARDWARE_CURSORS="1";NIXOS_OZONE_WL="1";};
-users.users.dx3d={isNormalUser=true;extraGroups=["wheel" "networkmanager" "video" "audio"];packages=with pkgs;[kitty hyprland waybar st dmenu vim asusctl fastfetch vesktop dwm ayugram-desktop librewolf xorg-server pavucontrol wofi yazi flatpak micro nitch wl-clipboard appimage-run git gh pkg-config hyprpaper gamemode htop xfce.thunar xfce.thunar-volman xfce.thunar-archive-plugin brightnessctl base16-schemes flameshot xclip];};
+users.users.dx3d={isNormalUser=true;extraGroups=["wheel" "networkmanager" "video" "audio"];packages=with pkgs;[kitty hyprland waybar st dmenu vim asusctl fastfetch vesktop dwm ayugram-desktop librewolf xorg-server pavucontrol wofi yazi flatpak micro nitch wl-clipboard appimage-run git gh pkg-config hyprpaper gamemode htop xfce.thunar xfce.thunar-volman xfce.thunar-archive-plugin brightnessctl base16-schemes flameshot xclip wireplumber];};
 programs.hyprland.enable=true;
 programs.steam={enable=true;remotePlay.openFirewall=true;dedicatedServer.openFirewall=true;};
 services.flatpak.enable=true;
@@ -23,6 +23,12 @@ services.udisks2.enable=true;
 services.picom={enable=true;backend="glx";vSync=true;activeOpacity=0.92;inactiveOpacity=0.85;fade=true;fadeDelta=5;settings={corner-radius=12;blur={method="dual_kawase";strength=5;};shadow=true;};};
 system.stateVersion="25.11";
 programs.bash.shellAliases={dotsync="cd ~/dotfiles && sudo cp /etc/nixos/configuration.nix . && sudo cp /etc/nixos/hardware-configuration.nix . && cp -r ~/.config/hypr . && cp -r ~/.config/waybar . && git add . && git commit -m \"update: $(date +'%Y-%m-%d %H:%M')\" && git push origin main && cd -";};
-nixpkgs.overlays=[(self: super: {dwm=super.dwm.overrideAttrs(oldAttrs:{postPatch="sed -i 's/#005577/#ff9e64/g' config.def.h";});})];
+nixpkgs.overlays=[(self: super: {dwm=super.dwm.overrideAttrs(oldAttrs:{postPatch=''
+sed -i '1i #include <X11/XF86keysym.h>' config.def.h
+sed -i "/static const char \*termcmd/a static const char *vdn[] = { \"wpctl\", \"set-volume\", \"@DEFAULT_AUDIO_SINK@\", \"5%-\", NULL };\nstatic const char *vup[] = { \"wpctl\", \"set-volume\", \"@DEFAULT_AUDIO_SINK@\", \"5%+\", NULL };\nstatic const char *mutmic[] = { \"wpctl\", \"set-mute\", \"@DEFAULT_AUDIO_SOURCE@\", \"toggle\", NULL };" config.def.h
+sed -i "/static const Key keys/a {0, XF86XK_AudioLowerVolume, spawn, {.v = vdn}}," config.def.h
+sed -i "/static const Key keys/a {0, XF86XK_AudioRaiseVolume, spawn, {.v = vup}}," config.def.h
+sed -i "/static const Key keys/a {0, XF86XK_AudioMicMute, spawn, {.v = mutmic}}," config.def.h
+'';});})];
 services.libinput={enable=true;touchpad.disableWhileTyping=true;};
 }
