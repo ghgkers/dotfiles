a1354821e17df1e741001469345cb7cbf701276e
dx3d <chayukov1024@gmail.com>
2026-04-28 09:50:29 +0000

    update: 2026-04-28 09:50

--- configuration.nix
+++ configuration.nix
@@ -12,9 +12,9 @@ services.asusd={enable=true;enableUserService=true;};
 services.xserver.xkb={layout="us,ru";variant="";options="grp:win_space_toggle";};
 i18n.defaultLocale="en_US.UTF-8";
 console={font="Lat2-Terminus16";keyMap="us";};
-hardware.nvidia.open=true;
+hardware.nvidia={open=true;modesetting.enable=true;};
 environment.sessionVariables={WLR_NO_HARDWARE_CURSORS="1";NIXOS_OZONE_WL="1";};
-users.users.dx3d={isNormalUser=true;extraGroups=["wheel" "networkmanager" "video" "audio"];packages=with pkgs;[kitty hyprland waybar st dmenu vim asusctl fastfetch vesktop dwm ayugram-desktop librewolf xorg-server pavucontrol wofi yazi flatpak micro nitch wl-clipboard appimage-run git gh pkg-config hyprpaper gamemode htop xfce.thunar xfce.thunar-volman xfce.thunar-archive-plugin brightnessctl base16-schemes flameshot xclip];};
+users.users.dx3d={isNormalUser=true;extraGroups=["wheel" "networkmanager" "video" "audio"];packages=with pkgs;[kitty hyprland waybar st dmenu vim asusctl fastfetch vesktop dwm ayugram-desktop librewolf xorg-server pavucontrol wofi yazi flatpak micro nitch wl-clipboard appimage-run git gh pkg-config hyprpaper gamemode htop xfce.thunar xfce.thunar-volman xfce.thunar-archive-plugin brightnessctl base16-schemes flameshot xclip config.hardware.nvidia.package.settings];};
 programs.hyprland.enable=true;
 programs.steam={enable=true;remotePlay.openFirewall=true;dedicatedServer.openFirewall=true;};
 services.flatpak.enable=true;
@@ -25,7 +25,7 @@ services.gvfs.enable=true;
 services.udisks2.enable=true;
 services.picom={enable=true;backend="glx";vSync=true;activeOpacity=0.92;inactiveOpacity=0.85;fade=true;fadeDelta=5;settings={corner-radius=12;blur={method="dual_kawase";strength=5;};shadow=true;shadow-radius=12;shadow-offset-x=-12;shadow-offset-y=-12;unredir-if-possible=true;};};
 boot.kernelPackages=pkgs.linuxPackages_zen;
-services.xserver={enable=true;windowManager.dwm.enable=true;};
+services.xserver={enable=true;windowManager.dwm.enable=true;displayManager.setupCommands=''${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 1920x1080 --rate 165'';};
 programs.bash.shellAliases={dotsync="cd ~/dotfiles && sudo cp /etc/nixos/configuration.nix . && sudo cp /etc/nixos/hardware-configuration.nix . && cp -r ~/.config/hypr . && cp -r ~/.config/waybar . && git add . && git commit -m \"update: $(date +'%Y-%m-%d %H:%M')\" && git push origin main && cd -";};
 nixpkgs.overlays=[(self: super: {dwm=super.dwm.overrideAttrs(oldAttrs:{postPatch=''sed -i 's/#005577/#ff9e64/g' config.def.h
 sed -i '1i #include <X11/XF86keysym.h>' config.def.h
