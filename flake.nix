d55d416aa7afc96389a020f5079a68fb51b20663
ghgkers <chayukov1024@gmail.com>
2026-05-09 16:19:38 +0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
-  description = "Minimal NixOS with DWM, st, Librewolf";
+  description = "Ultimate NixOS Gaming + sowm (CachyOS kernel)";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    # Stable base, but we'll overlay CachyOS kernel for speed
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-  outputs = { self, nixpkgs }: {
+    # Chaotic Nyx – provides CachyOS kernel & extra gaming packages
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, chaotic, ... }@inputs: {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        chaotic.nixosModules.default
+        # Enable the chaotic overlay (so we can use linuxPackages_cachyos-lto)
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [ chaotic.overlays.default ];
+        })
+      ];
     };
   };
 }
