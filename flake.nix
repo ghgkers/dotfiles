099c86d09b62149f0196735f99323149d30d7309
ghgkers <chayukov1024@gmail.com>
2026-05-09 13:46:57 +0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,8 @@
   description = "NixOS configuration with CachyOS kernel and sowm";
 
   inputs = {
-    # Use the NixOS 25.11 branch for stability with stateVersion 25.11
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    # If you don't use stylix yet, you can remove the next two lines
-    # stylix.url = "github:danth/stylix";
   };
 
   outputs = { self, nixpkgs, chaotic, ... }@inputs: {
@@ -15,7 +12,6 @@
       modules = [
         ./configuration.nix
         chaotic.nixosModules.default
-        # stylix.nixosModules.stylix   # uncomment only if you add stylix config
       ];
     };
   };
