2d2bb2f43587a71e85694798f99cdb5096a07a74
ghgkers <chayukov1024@gmail.com>
2026-06-26 08:56:51 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,19 @@
   description = "Ultimate Portable NixOS Gaming Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";[cite: 2]
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";[cite: 2]
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, chaotic, ... }@inputs: {
     nixosConfigurations = {
       nix-gaming = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };[cite: 2]
+        specialArgs = { inherit inputs; };
         modules = [
           ./hosts/rog-strix/hardware-configuration.nix
           ./hosts/rog-strix/hardware-spec.nix
           ./configuration.nix
-          chaotic.nixosModules.default[cite: 2]
+          chaotic.nixosModules.default
         ];
       };
     };
